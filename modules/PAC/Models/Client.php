26f9ae23a7fbcc9cacce8916463613699284c92b
Lazaro Raisel Milians Alvarez de la Campa <lazaro.milians@numencorp.com>
2014-06-19 11:10:32 -0500

    Agregando los campos en el metodo Process de la clase Client

--- modules/PAC/Models/Client.php
+++ modules/PAC/Models/Client.php
@@ -43,8 +43,8 @@ Class PAC_Client_Model {
         $email = $request->get('email');
         $alternativeEmail = $request->get('secondaryemail');
         $webSite = $request->get('pac_sitio_web');
-        $longitude = $request->get('pac_longitud');
-        $latitude = $request->get('pac_latitud');
+        /*$longitude = $request->get('pac_longitud');
+        $latitude = $request->get('pac_latitud');*/
         $location = $request->get('pac_localidad');
         $counter = $request->get('pac_pagador_contador');
 
@@ -57,13 +57,98 @@ Class PAC_Client_Model {
         $paymentCondition = $request->get('pac_precios');
         $creditLimit = $request->get('pac_limite_credito');
         $expiredQuota = $request->get('pac_cuotas_vencida');
-        $expiredDay = $request->get('pac_cuotas_vencida');
+        $expiredDay = $request->get('pac_dias_vencidos');
+        $limitedAmount = $request->get('pac_limite_cantidad');
+        $discount = $request->get('pac_descuento');
+        $promotions = $request->get('pac_promociones');
+        $protestedChecks = $request->get('pac_cheques_protestados');
+        $observations = $request->get('pac_observaciones');
+        $allowAccess = $request->get('pac_permitir_acceso');
+        $company = $request->get('pac_empresa');
+        $supplierCode = $request->get('pac_cod_provedor');
+        $partialClearance = $request->get('pac_despacho_parcial');
+        $withIVA = $request->get('pac_con_iva');
+        $printBarcode = $request->get('pac_imprimir_barcod');
+        $accountCountable = $request->get('pac_cuenta_contable');
+        
+       // $image = $request->get('pac_xxxxxx');
+        $companyName = $request->get('pac_razon_social');
+        $legalRepresentative = $request->get('pac_representante');
+        $rucRepresentative = $request->get('pac_ruc_representante');
+        $businessTime = $request->get('pac_tiempo_negocio');
+        $addressRepresentative = $request->get('pac_direccion_representante');
+        $phoneRepresentative = $request->get('pac_telefono_representante');
+        
+        $bankReferences = $request->get('pac_referencias_bancarias');
+        $tradeReferences = $request->get('pac_referencias_comerciales');
+        $creditCards = $request->get('pac_tarjetas_credito');
+       // $authorizedSignatures = $request->get('pac_xxxxxxx');
+        
+        $promissoryNote = $request->get('pac_pagare');
+        $promissoryNoteValue = $request->get('pac_valor_pagare');
+        $guarantor = $request->get('pac_garante');
+        $expirationDate = $request->get('pac_fecha_vencimiento');
+        
+        $holderStatus = $request->get('pac_estado_soporte');    
+        $ticketNumber = $request->get('pac_numero_ticket');
+        
+        
+        
         
 
         $recordId = $request->get('record');
 
         if (empty($recordId)) {
-            $sql = "INSERT INTO maecte (codcte01,nomcte01,dircte01,telcte01,cascte01,emailcte01) VALUES ('" . $code . "','" . $name . "','" . $address . "','" . $homePhone . "','" . $cedulaRuc . "','" . $email . "') ;";
+            $sql = "INSERT INTO maecte (codcte01,nomcte01,dircte01,telcte01,cascte01,emailcte01,emailaltcte01,website,zoom01,pagleg01,ofienccte01,"
+                    . "vendcte01,cobrcte01,cordcte01,precte01,condpag01,limcred01,cuotasven01,diasven01,limcant01,desctocte01,promocte01,cheqpro01,"
+                    . "obsercte01,acceder,idemp01,codprov01,desppar01,coniva01,cv2cte01,ctacgcte01,razcte01,repleg01,ruc01,timenegocio,dircliente01,"
+                    . "telcte01c,refbanc01,refcom01,tarjcred01,pagare01,valorpagare01,garante01,fecvenp01,estsop01,notick01) "
+                    . "VALUES ('" . $code . "',"
+                    . "'" . $name . "',"
+                    . "'" . $address . "',"
+                    . "'" . $homePhone . "',"
+                    . "'" . $cedulaRuc . "',"
+                    . "'" . $email . "',"
+                    . "'" . $alternativeEmail . "',"
+                    . "'" . $webSite . "',"
+                    . "'" . $location . "',"
+                    . "'" . $counter . "',"
+                    . "'" . $responsibleOffice . "',"
+                    . "'" . $vendor . "',"
+                    . "'" . $billCollector . "',"
+                    . "'" . $coordinator . "',"
+                    . "'" . $price . "',"
+                    . "'" . $paymentCondition . "',"
+                    . "'" . $creditLimit . "',"
+                    . "'" . $expiredQuota . "',"
+                    . "'" . $expiredDay . "',"
+                    . "'" . $limitedAmount . "',"
+                    . "'" . $discount . "',"
+                    . "'" . $promotions . "',"
+                    . "'" . $protestedChecks . "',"
+                    . "'" . $observations . "',"
+                    . "'" . $allowAccess . "',"
+                    . "'" . $company . "',"
+                    . "'" . $supplierCode . "',"
+                    . "'" . $partialClearance . "',"
+                    . "'" . $withIVA . "',"
+                    . "'" . $printBarcode . "',"
+                    . "'" . $accountCountable . "',"
+                    . "'" . $companyName . "',"
+                    . "'" . $legalRepresentative . "',"
+                    . "'" . $rucRepresentative . "',"
+                    . "'" . $businessTime . "',"
+                    . "'" . $addressRepresentative . "',"
+                    . "'" . $phoneRepresentative . "',"
+                    . "'" . $bankReferences . "',"
+                    . "'" . $tradeReferences . "',"
+                    . "'" . $creditCards . "',"
+                    . "'" . $promissoryNote . "',"
+                    . "'" . $promissoryNoteValue . "',"
+                    . "'" . $guarantor . "',"
+                    . "'" . $expirationDate . "',"
+                    . "'" . $holderStatus . "',"
+                    . "'" . $ticketNumber . "') ;";
         } else {
             $sql = "UPDATE maecte SET nomcte01 = '" . $name . "', dircte01 = '" . $address . "', telcte01 =  '" . $homePhone . "', cascte01 = '" . $cedulaRuc . "', emailcte01 = '" . $email . "' WHERE codcte01 = '" . $code . "';";
         }
