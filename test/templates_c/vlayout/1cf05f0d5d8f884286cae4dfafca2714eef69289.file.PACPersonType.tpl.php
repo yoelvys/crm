cc03619c234cbc4128d46ceba117f035ffb97069
Lazaro Raisel Milians Alvarez de la Campa <lazaro.milians@numencorp.com>
2014-06-20 12:24:04 -0500

    Arreglando variables del UpdateAjax

--- test/templates_c/vlayout/1cf05f0d5d8f884286cae4dfafca2714eef69289.file.PACPersonType.tpl.php
+++ test/templates_c/vlayout/1cf05f0d5d8f884286cae4dfafca2714eef69289.file.PACPersonType.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2014-06-19 16:18:08
+<?php /* Smarty version Smarty-3.1.7, created on 2014-06-20 16:58:14
          compiled from "C:\xampp\htdocs\crm\includes\runtime/../../layouts/vlayout\modules\Vtiger\uitypes\PACPersonType.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:10430539b133e3a4cf1-15447818%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '1cf05f0d5d8f884286cae4dfafca2714eef69289' => 
     array (
       0 => 'C:\\xampp\\htdocs\\crm\\includes\\runtime/../../layouts/vlayout\\modules\\Vtiger\\uitypes\\PACPersonType.tpl',
-      1 => 1403194253,
+      1 => 1403283414,
       2 => 'file',
     ),
   ),
@@ -28,5 +28,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <?php if ($_valid && !is_callable('content_539b133e3d3e3')) {function content_539b133e3d3e3($_smarty_tpl) {?>
 <?php $_smarty_tpl->tpl_vars["FIELD_INFO"] = new Smarty_variable(Vtiger_Util_Helper::toSafeHTML(Zend_Json::encode($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldInfo())), null, 0);?><?php $_smarty_tpl->tpl_vars["SPECIAL_VALIDATOR"] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getValidator(), null, 0);?><?php $_smarty_tpl->tpl_vars['FIELD_NAME'] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('name'), null, 0);?><?php $_smarty_tpl->tpl_vars['FIELD_VALUE'] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue'), null, 0);?><?php if ($_smarty_tpl->tpl_vars['FIELD_VALUE']->value==''){?><select class="chzn-select <?php echo $_smarty_tpl->tpl_vars['FIELD_NAME']->value;?>
 " name = '<?php echo $_smarty_tpl->tpl_vars['FIELD_NAME']->value;?>
-'><option value="Natural">Natural</option><option value="Jurídica">Jurídica</option></select><?php }else{ ?><span name ="pac_tipo_persona"><?php echo $_smarty_tpl->tpl_vars['FIELD_VALUE']->value;?>
+'><option value="Natural">Natural</option><option value="Jurídica">Jurídica</option></select><?php }else{ ?><input value="<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
+" type="hidden" class="fieldname" name="<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldName();?>
+"/><span name ="pac_tipo_persona"><?php echo $_smarty_tpl->tpl_vars['FIELD_VALUE']->value;?>
 </span><?php }?><?php }} ?>
--- test/templates_c/vlayout/de7e3a138bfe2e9befdfff88d4417e00d344bd17.file.PACCode.tpl.php
+++ test/templates_c/vlayout/de7e3a138bfe2e9befdfff88d4417e00d344bd17.file.PACCode.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2014-06-19 16:18:09
+<?php /* Smarty version Smarty-3.1.7, created on 2014-06-20 16:58:15
          compiled from "C:\xampp\htdocs\crm\includes\runtime/../../layouts/vlayout\modules\Vtiger\uitypes\PACCode.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:3033053a30d41227799-38185839%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'de7e3a138bfe2e9befdfff88d4417e00d344bd17' => 
     array (
       0 => 'C:\\xampp\\htdocs\\crm\\includes\\runtime/../../layouts/vlayout\\modules\\Vtiger\\uitypes\\PACCode.tpl',
-      1 => 1403194253,
+      1 => 1403283414,
       2 => 'file',
     ),
   ),
@@ -15,6 +15,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_53a30d41259b7',
   'variables' => 
   array (
     'FIELD_MODEL' => 0,
@@ -23,8 +25,6 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'SPECIAL_VALIDATOR' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.7',
-  'unifunc' => 'content_53a30d41259b7',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_53a30d41259b7')) {function content_53a30d41259b7($_smarty_tpl) {?><?php $_smarty_tpl->tpl_vars["FIELD_INFO"] = new Smarty_variable(Vtiger_Util_Helper::toSafeHTML(Zend_Json::encode($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldInfo())), null, 0);?><?php $_smarty_tpl->tpl_vars["SPECIAL_VALIDATOR"] = new Smarty_variable($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getValidator(), null, 0);?><?php if ($_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue')==''){?><input id="<?php echo $_smarty_tpl->tpl_vars['MODULE']->value;?>
 _editView_fieldName_<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('name');?>
@@ -32,5 +32,7 @@ _editView_fieldName_<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get
 "value="<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
 "  data-fieldinfo='<?php echo $_smarty_tpl->tpl_vars['FIELD_INFO']->value;?>
 ' <?php if (!empty($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value)){?>data-validator=<?php echo Zend_Json::encode($_smarty_tpl->tpl_vars['SPECIAL_VALIDATOR']->value);?>
-<?php }?> /><?php }else{ ?><span><?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
+<?php }?> /><?php }else{ ?><input value="<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
+" type="hidden" class="fieldname" name="<?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->getFieldName();?>
+"/><span><?php echo $_smarty_tpl->tpl_vars['FIELD_MODEL']->value->get('fieldvalue');?>
 </span><?php }?><?php }} ?>
